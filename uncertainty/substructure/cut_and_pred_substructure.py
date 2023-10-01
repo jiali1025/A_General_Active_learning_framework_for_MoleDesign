e8220ce3f44721f5e40de7f5ff40f5699f1ad4a6
yinhy19 <69662122+yinhy19@users.noreply.github.com>
2023-10-01 20:59:55 +0800

    Add files via upload

---
+++ uncertainty/substructure/cut_and_pred_substructure.py
@@ -0,0 +1,80 @@
+'''
+This code is used to cut and pred the total data and generate the new train set.
+First, slice the total data into smaller to avoid memory shortage.
+Second, predict the data and merge the data.
+Then, based on the most uncertain 20000 moleculars, generate the new train set for training.
+Also, generate new total data for the next round.
+'''
+
+def cut_and_pred_substructure(dt_tot_csv, cut_len, round, check_point_path):
+    import math
+    import os
+    import pandas as pd
+    import time
+    from tqdm import tqdm
+    from rdkit import Chem
+
+    os.mkdir(f'../cut_data/origin_cut/round_{round}')
+    os.mkdir(f'../cut_data/pred_cut/round_{round}')
+    substructure_emit = pd.read_csv('emit_substructure.csv')['smiles']
+    substructure_sens = pd.read_csv('sens_substructure.csv')['smiles']
+    substructure_tot = pd.concat([substructure_emit, substructure_sens])
+    
+    dt_tot = pd.read_csv(dt_tot_csv)
+    dt_tot.reset_index(drop = True, inplace = True)
+    tot_len = len(dt_tot)
+    file_num = math.ceil(tot_len / cut_len)
+    for i in range(file_num):
+        if i != (file_num - 1):
+            dt_tot.iloc[i*cut_len:(i+1)*cut_len, :].to_csv(f'../cut_data/origin_cut/round_{round}/dt_tot_{i}.csv')
+        if i == (file_num - 1):
+            dt_tot.iloc[i*cut_len:, :].to_csv(f'../cut_data/origin_cut/round_{round}/dt_tot_{i}.csv')
+    for i in range(file_num):
+        print(i)
+        test_path = f'../cut_data/origin_cut/round_{round}/dt_tot_{i}.csv'
+        pred_path = f'../cut_data/pred_cut/round_{round}/dt_tot_{i}.csv'
+        pred = f'dt_tot_{i}.csv'
+        k = 0
+        while pred not in os.listdir(f'../cut_data/pred_cut/round_{round}'):
+            os.system(f'chemprop_predict --test_path {test_path} --checkpoint_dir {check_point_path} --preds_path {pred_path} --smiles_column SMILES --ensemble_variance')
+            time.sleep(10)
+            if k >= 1:
+                print(f'data{i} fail {k} time(s)')
+            k = k + 1
+            
+    dt_list = []
+    for i in range(file_num):
+        dt_path = f'../cut_data/pred_cut/round_{round}/dt_tot_{i}.csv'
+        dt = pd.read_csv(dt_path)
+        dt_list.append(dt)
+    pred_tot = pd.concat(dt_list).dropna()
+    pred_tot.reset_index(drop = True, inplace = True)
+    pred_tot = pred_tot[~pred_tot['xTB_S1'].isin(['Invalid SMILES'])]
+    pred_tot['uncertainty_tot'] = pred_tot['xTB_S1_ensemble_uncal_var'].apply(lambda x: float(x)) + pred_tot['xTB_T1_ensemble_uncal_var'].apply(lambda x: float(x))
+    pred_tot.sort_values(by = 'uncertainty_tot', ascending = False, inplace = True)
+
+    suggest_list = []
+    pred_len = len(pred_tot)
+    smiles_tot = pred_tot['SMILES']
+
+    for i in tqdm(range(pred_len)):
+        mol = Chem.MolFromSmiles(smiles_tot[i])
+        for sub in substructure_tot:
+            sub_mol = Chem.MolFromSmiles(sub)
+            if mol.HasSubstructMatch(sub_mol):
+                suggest_list.append(i)
+                break
+        if len(suggest_list) >= 20000:
+            break
+        if (len(suggest_list) % 200) == 0:
+            print(f'suggest list size is {len(suggest_list)}')
+
+    pred_index = pred_tot.index[suggest_list]
+    newtrain = dt_tot.iloc[pred_index, :]
+    train_set = pd.read_csv(f'../train_set/train_round{round}.csv')
+    train_set = pd.concat([newtrain, train_set])
+    train_set.to_csv(f'../train_set/train_round{round+1}.csv')
+
+    dt_tot_new = dt_tot.iloc[list(set(pred_tot.index)-set(pred_index)), :]
+    dt_tot_new.reset_index(drop = True, inplace = True)
+    dt_tot_new.to_csv(f'../data_tot/dt_tot_round{round+1}.csv')
