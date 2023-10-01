af0ed9ebed04412a4a7595bc351c73a114f17ff1
yinhy19 <69662122+yinhy19@users.noreply.github.com>
2023-10-01 21:03:50 +0800

    Delete Unified_Active_Learning_Framework directory

--- Unified_Active_Learning_Framework/Generative_model_training/check.py
+++
@@ -1,3 +0,0 @@
-import torch
-model_state, optimizer_state, total_step, beta = torch.load('temp.ckpt')
-print('cool')
--- Unified_Active_Learning_Framework/Pretrain_chemprop_singlecard_code_only/chemprop-master/chemprop/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .model_mask import MoleculeModel
-from .mpn_mask import MPN, MPNEncoder
-
-__all__ = [
-    'MoleculeModel',
-    'MPN',
-    'MPNEncoder'
-]
